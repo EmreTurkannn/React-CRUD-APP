1789c4b9a025baacbbf0c39ef0bea90258106361
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-03 00:01:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Home from './component/pages/Home'
+import About from './component/pages/About'
+import Contact from './component/pages/Contact'
+import Navbar from './component/pages/layout/Navbar'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+        <Home/>
+        <About/>
+        <Contact/>
     </div>
   );
 }
