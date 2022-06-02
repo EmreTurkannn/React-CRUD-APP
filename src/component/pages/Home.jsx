d0dbc44266c6bd4507b2c46841c6292c22ca2944
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-03 00:02:52 +0300

    home component uptade

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home=()=>{
     return(
         <div className='container'>
-                <h1>Home Page</h1>
+                <h1>Homes Page</h1>
         </div>
     )
 }
