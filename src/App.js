b8a10e10d8df816440bcd3342a700371d214835b
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-09 10:46:39 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/React-CRUD-APP" element={<Home/>}>
+          <Route path="/" element={<Home/>}>
           </Route>
           <Route path="/about" element={<About/>}>
           </Route>
